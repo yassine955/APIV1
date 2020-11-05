32f3fb777c790d13cedb0cb27db7b6dc76bf2d11
youngkesinova <youngkesinova@gmail.com>
2020-11-05 17:03:37 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,5 +5,11 @@ declare namespace NodeJS {
     SESSION_SECRET: string;
     CORS_ORIGIN: string;
     DATABASE_URL: string;
+    CLIET_ID: string;
+    CLIENT_SECRET: string;
+    callbackUrl: string;
+    redirectUrl: string;
+    BOT_TOKEN: string;
+    API_URI: string;
   }
 }
