4dc3dd10170564998732240e688735375c3db72c
youngkesinova <youngkesinova@gmail.com>
2020-11-05 17:03:41 +0100

    Update Guild.ts

--- src/entities/Guild.ts
+++ src/entities/Guild.ts
@@ -1,38 +1,50 @@
 import { ObjectType, Field } from "type-graphql";
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  OneToOne,
+  JoinColumn,
+  PrimaryColumn,
+} from "typeorm";
+import { Settings } from "./Settings";
 
 @ObjectType()
 @Entity()
 export class Guild extends BaseEntity {
   @Field()
-  @PrimaryGeneratedColumn()
-  id!: number;
+  @PrimaryColumn()
+  id: string;
 
   @Field()
   @Column()
-  serverName!: string;
+  name: string;
+
+  @Field()
+  @Column({ nullable: true })
+  settingsId: string;
+
+  @Field()
+  @Column({ nullable: true })
+  icon: string;
 
   @Field()
   @Column()
-  guildId!: string;
+  owner: boolean;
 
-  @Field()
-  @Column()
-  ownerId!: string;
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  ownerID: string;
 
-  @Field()
-  @Column()
-  prefix!: string;
+  @Column({ nullable: true })
+  permissions: number;
 
-  @Field()
-  @Column()
-  hadithChannel!: string;
+  @Column({ nullable: true })
+  permissions_new: string;
 
-  @Field()
-  @Column()
-  hijriChannel!: string;
-
-  @Field()
-  @Column()
-  hadithLanguage!: string;
+  @Field({ nullable: true })
+  @OneToOne(() => Settings, () => {}, { cascade: true, onDelete: "CASCADE" })
+  @JoinColumn()
+  settings: Settings;
 }
